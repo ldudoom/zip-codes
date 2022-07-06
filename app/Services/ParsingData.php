df129880ecf6f275873688083e5ab90451d49ec7
Raul Chauvin <raul.chauvin@telefonica.com>
2022-07-06 03:11:46 -0500

    Quitando caracteres especiales

--- app/Services/ParsingData.php
+++ app/Services/ParsingData.php
@@ -7,7 +7,8 @@ class ParsingData implements \App\Interfaces\ParsingDataInterface
 
     public function fieldParsed(array $zipCodeDetail): array
     {
-        $zipCodeExploded = $this->_explodeZipCodeInfo($zipCodeDetail);
+        $zipCodeWithoutSpecialChars = array_map([$this, '_removeSpecialChars'], $zipCodeDetail);
+        $zipCodeExploded = $this->_explodeZipCodeInfo($zipCodeWithoutSpecialChars);
         return $this->_parsingDataStructure($zipCodeExploded);
     }
 
@@ -23,26 +24,63 @@ class ParsingData implements \App\Interfaces\ParsingDataInterface
         $aZipCodes = [];
         foreach($zipCodesExploded as $zipCodes){
             $aZipCodes['zip_code'] = $zipCodes[0];
-            $aZipCodes['locality'] = $zipCodes[5];
+            $aZipCodes['locality'] = strtoupper($zipCodes[5]);
             $aZipCodes['federal_entity'] = array(
                 'key' => (int)$zipCodes[7],
-                'name' => $zipCodes[4],
+                'name' => strtoupper($zipCodes[4]),
                 'code' => $zipCodes[9] != '' ? (int)$zipCodes[9] : null
             );
             $aZipCodes['settlements'][] = [
                 'key' => (int)$zipCodes[12],
-                'name' => $zipCodes[1],
-                'zone_type' => $zipCodes[13],
+                'name' => strtoupper($zipCodes[1]),
+                'zone_type' => strtoupper($zipCodes[13]),
                 'settlement_type' => [
                     'name' => $zipCodes[2],
                 ]
             ];
             $aZipCodes['municipality'] = [
                 'key' => (int)$zipCodes[11],
-                'name' => $zipCodes[3]
+                'name' => strtoupper($zipCodes[3])
             ];
         }
         return $aZipCodes;
+    }
+
+    private function _removeSpecialChars(string $string): string
+    {
+        $string = str_replace(
+            array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+            array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+            $string
+        );
+
+        $string = str_replace(
+            array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+            array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+            $string );
+
+        $string = str_replace(
+            array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+            array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+            $string );
+
+        $string = str_replace(
+            array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+            array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+            $string );
+
+        $string = str_replace(
+            array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+            array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+            $string );
+
+        $string = str_replace(
+            array('Ñ', 'ñ', 'Ç', 'ç'),
+            array('N', 'n', 'C', 'c'),
+            $string
+        );
+
+        return $string;
     }
 
 }
